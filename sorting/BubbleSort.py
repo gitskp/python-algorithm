11d3e3f7fc6d5d51d1fd49fd70b0477c7f01f224
Shrikant Pandey <shrikantpandeymnnit2015@gmail.com>
2019-05-15 16:51:55 +0530

    Update BubbleSort.py

--- sorting/BubbleSort.py
+++ sorting/BubbleSort.py
@@ -17,3 +17,21 @@ arr = [60,50,40,30,20,10,]
 n = len(arr)
 print(BubbleSort(arr,n))
 
+
+
+"""
+
+
+Worst and Average Case Time Complexity: O(n*n). Worst case occurs when array is reverse sorted.
+
+Best Case Time Complexity: O(n). Best case occurs when array is already sorted.
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are already sorted.
+
+Sorting In Place: Yes
+
+Stable: Yes
+
+"""
